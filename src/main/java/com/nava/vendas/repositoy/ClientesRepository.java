9ae08bb77da8b2a7634d75fdf913534e917e45e8
flino1934 <f.lino1934@hotmail.com>
2021-05-10 00:31:49 -0300

    Teste basicos no spring

---
+++ src/main/java/com/nava/vendas/repositoy/ClientesRepository.java
@@ -0,0 +1,20 @@
+package com.nava.vendas.repositoy;
+
+import org.springframework.stereotype.Repository;
+
+import com.nava.vendas.model.Cliente;
+
+@Repository
+public class ClientesRepository {
+
+	public void persistir(Cliente cliente) {
+		// TODO Auto-generated method stub
+		System.out.println("Salvar");
+		
+	}
+
+	
+
+	
+	
+}

d8884a7a97d43cfadd4272e20290157a3599e7af
flino1934 <f.lino1934@hotmail.com>
2021-05-10 22:26:02 -0300

    mapeando a classe de Cliente para JPA

---
+++ src/main/java/com/nava/vendas/domain/entity/ItemPedido.java
@@ -0,0 +1,46 @@
+package com.nava.vendas.domain.entity;
+
+public class ItemPedido {
+
+	private Integer id;
+	private Pedido pedido;
+	private Produto produto;
+	private Integer quantidade;
+
+	public ItemPedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
---
+++ src/main/java/com/nava/vendas/domain/repository/ClientesRepository.java
@@ -0,0 +1,8 @@
+package com.nava.vendas.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository//esta anotado como uma classe de repositorio a qual vai acessar a base de dados apos uma requisição da classe service
+public class ClientesRepository {
+
+}

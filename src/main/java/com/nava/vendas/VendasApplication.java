33ae7b4f5b9dace473138545a844d38f1ee74edd
flino1934 <f.lino1934@hotmail.com>
2021-05-12 00:03:11 -0300

    Migrando para o desenvolvimento rest

--- src/main/java/com/nava/vendas/VendasApplication.java
+++ src/main/java/com/nava/vendas/VendasApplication.java
@@ -1,50 +1,12 @@
 package com.nava.vendas;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.nava.vendas.domain.entity.Cliente;
-import com.nava.vendas.domain.entity.Pedido;
-import com.nava.vendas.domain.repository.ClientesRepository;
-import com.nava.vendas.domain.repository.PedidoRepository;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
-
-	@Bean
-	public CommandLineRunner init(@Autowired ClientesRepository clientesRepository,
-			@Autowired PedidoRepository pedidosRepository) {
-
-		return args -> {
-
-			System.out.println("========= Salvando clientes! =========");
-			Cliente fulano = new Cliente("Felipe Lino");
-			clientesRepository.save(fulano);//esta salavando o cliente
-
-			Pedido p = new Pedido();
-			p.setCliente(fulano);// esta pedido como parametro um objeto do tipo Cleiente
-			p.setDataPedido(LocalDate.now());
-			p.setTotal(BigDecimal.valueOf(100));
-			pedidosRepository.save(p);//esta salvando os pedidos
-
-//			Cliente cliente = clientesRepository.findClienteFetchPedidos(fulano.getId());
-//			System.out.println(cliente);
-//			System.out.println(cliente.getPedido());
-
-			pedidosRepository.findByCliente(fulano).forEach(System.out::println);//esta pedquisando todos os pedidos de um determinado cliente 
-			
-		};
-
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/com/nava/vendas/domain/repository/PedidoRepository.java
+++ src/main/java/com/nava/vendas/domain/repository/PedidoRepository.java
@@ -1,7 +1,6 @@
 package com.nava.vendas.domain.repository;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +9,8 @@ import com.nava.vendas.domain.entity.Cliente;
 import com.nava.vendas.domain.entity.Pedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
-	
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
 	List<Pedido> findByCliente(Cliente cliente);
-	
 
 }

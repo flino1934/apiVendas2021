99035becc6d57b6d35224ab4f0e23d2b40d3525a
flino1934 <f.lino1934@hotmail.com>
2021-05-16 23:42:12 -0300

    PasswordEncoder

--- src/main/java/com/nava/vendas/config/SecurityConfig.java
+++ src/main/java/com/nava/vendas/config/SecurityConfig.java
@@ -1,16 +1,30 @@
 package com.nava.vendas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();// toda vez que o usuario passar uma senha ele vai gerar um hash diferente da senha
+
+			
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {//esse metodo vai gerenciar os objetos de autenticação no contexto do security, aqui será configurado a autenticação exemplo qual vai ser o usuario qual a senha de onde vem 
-		// TODO Auto-generated method stub
+		
+		
+		
+		
 		super.configure(auth);
 	}
 
